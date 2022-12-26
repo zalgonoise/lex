bf5e2e27c4be652f519ba75fa6a916eb8bb5c965
zalgonoise <zalgo.noise@gmail.com>
2022-12-27 00:45:58 +0100

    item: add position element

--- item.go
+++ item.go
@@ -2,13 +2,15 @@ package lex
 
 // Item represents a set of any type of tokens identified by a comparable type
 type Item[T comparable, V any] struct {
+	Pos   int
 	Type  T
 	Value []V
 }
 
 // NewItem creates an Item with type `T` and values `[]V`
-func NewItem[T comparable, V any](itemType T, value ...V) Item[T, V] {
+func NewItem[T comparable, V any](pos int, itemType T, value ...V) Item[T, V] {
 	return Item[T, V]{
+		Pos:   pos,
 		Type:  itemType,
 		Value: value,
 	}
